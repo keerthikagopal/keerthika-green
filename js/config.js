dafee1324362c63340d66534db9b1e96b702f28f
keerthikagopal <keerthikaggopal@gmail.com>
2025-11-11 11:55:34 +0530

    Update Cognito userPoolId and userPoolClientId

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_NPDfYHOql', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4sojldlivmgv1fvb4dp9063mgc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_NzKakf76p', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7eh0ll2k3gqlapai9j4cbakvqg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://a6ew8stgxg.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
