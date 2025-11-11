0be85f99b054ae4d76da027818ca090bc7bf6c2d
keerthikagopal <keerthikaggopal@gmail.com>
2025-11-11 10:53:43 +0530

    Update Cognito configuration in config.js
    Updated Cognito user pool ID and client ID in the configuration.

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_y578hBejS', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '72gduv81m0jrfsmob4ppmgmbpf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_NPDfYHOql', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4sojldlivmgv1fvb4dp9063mgc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://ralmchxygd.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
