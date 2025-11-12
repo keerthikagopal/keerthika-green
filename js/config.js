e01db9fbf34fcd9dd4c53eaf1bd47c651cd4c617
keerthikagopal <keerthikaggopal@gmail.com>
2025-11-12 11:00:14 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_NzKakf76p', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7eh0ll2k3gqlapai9j4cbakvqg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_AYt8oAuya', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '163n9vhvg3gpm5l9cb7m7f4ccs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://n1k9t7ved7.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
